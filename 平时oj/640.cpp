714b13634e83cbb4cc279c3955798b95d9bbfc25
Eularcat <141126420+CATEular@users.noreply.github.com>
2023-12-12 21:30:25 +0800

    Create 640.cpp

---
+++ 平时oj/640.cpp
@@ -0,0 +1,40 @@
+//删除两两相邻的重复数字
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int b[114514],lth = 0,t = 0;
+int main()
+{
+	stack<int> stk;
+	int n; cin >> n;
+	while(n--)
+	{
+		int a;cin >> a;
+		if(stk.empty()) 
+		{
+			stk.push(a);
+			lth++;
+		}
+		else
+		{
+			if(a == stk.top()) 
+			{
+				stk.pop();
+				lth--;
+			}
+			else 
+			{
+				stk.push(a);
+				lth++;
+			}
+		}
+	}
+	if(stk.empty()) cout << "NULL";
+	while(!stk.empty())
+	{
+		b[++t] = stk.top();
+		stk.pop();
+	}
+	for(int i = lth ; i > 0 ; i--) cout << b[i] << ' ';
+}
